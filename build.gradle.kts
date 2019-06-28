63cc1ddeb47c295b7a5d1e80a275ea2dc0f8c36f
Don Nguyen <ndon55555@gmail.com>
2019-06-27 22:50:19 -0400

    Migrate project from Java 8 to Java 12
    - JavaFX no longer is part of Java so it has to be grabbed from a separate repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     java
     kotlin("jvm") version "1.3.40"
+    id("org.openjfx.javafxplugin") version "0.0.7"
 }
 
 group = "tetris"
@@ -20,7 +21,14 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
 }
 
+apply(plugin = "org.openjfx.javafxplugin")
+
+javafx {
+    modules("javafx.controls", "javafx.fxml")
+}
+
 configure<JavaPluginConvention> {
+    version = "12.01"
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
 
@@ -28,5 +36,5 @@ sourceSets["main"].java.srcDirs("src")
 sourceSets["test"].java.srcDirs("test")
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "12"
 }
