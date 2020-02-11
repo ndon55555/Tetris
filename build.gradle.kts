0bc74f3e4a41844a25efced0eb74ccb301c3ee23
Don Nguyen <ndon55555@gmail.com>
2020-02-10 22:14:53 -0500

    eliminate dead code for deployed files

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,14 @@ val browser by extra("src/browser")
 val webDir by extra("${projectDir}/web")
 val desktop by extra("src/desktop")
 
+val devMode = (System.getenv("DEV_MODE") ?: "true").toBoolean()
+
 plugins {
     kotlin("multiplatform") version "1.3.61"
+}
+
+apply {
+    plugin("kotlin-dce-js")
 }
 
 repositories {
@@ -34,7 +40,6 @@ kotlin {
     js {
         val main by compilations.getting {
             kotlinOptions {
-                sourceMap = true
                 moduleKind = "umd"
             }
         }
@@ -115,22 +120,26 @@ kotlin {
         register<Copy>("assembleJs") {
             dependsOn("jsMainClasses")
 
-            val jsJar = { regex: String ->
-                configurations.getByName("jsRuntimeClasspath").single {
-                    it.name.matches(Regex(regex))
+            if (devMode) {
+                val jsJar = { regex: String ->
+                    configurations.getByName("jsRuntimeClasspath").single {
+                        it.name.matches(Regex(regex))
+                    }
                 }
-            }
 
-            val stdLib = jsJar("kotlin-stdlib-js-.+\\.jar")
-            val kotlinxHtml = jsJar("kotlinx-html-js-.+\\.jar")
+                val stdLib = jsJar("kotlin-stdlib-js-.+\\.jar")
+                val kotlinxHtml = jsJar("kotlinx-html-js-.+\\.jar")
+                from(zipTree(stdLib), zipTree(kotlinxHtml), getByName("compileKotlinJs"))
+                include { fileTreeElement ->
+                    val path = fileTreeElement.path
+                    (path.endsWith(".js") || path.endsWith(".js.map")) &&
+                        (path.startsWith("META-INF/resources/") || !path.startsWith("META-INF/"))
+                }
+            } else {
+                from(getByName("runDceJsKotlin"))
+            }
 
             includeEmptyDirs = false
-            from(zipTree(stdLib), zipTree(kotlinxHtml), getByName("compileKotlinJs"))
-            include { fileTreeElement ->
-                val path = fileTreeElement.path
-                (path.endsWith(".js") || path.endsWith(".js.map")) &&
-                    (path.startsWith("META-INF/resources/") || !path.startsWith("META-INF/"))
-            }
             into(webDir)
         }
     }
