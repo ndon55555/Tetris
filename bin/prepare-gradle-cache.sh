470b07773a19285fc39441c719001b1710a03eef
Don Nguyen <ndon55555@gmail.com>
2020-02-18 23:34:10 -0500

    don't delete README.md from deployment folder
    - use Travis-recommended way of caching Gradle

--- bin/prepare-gradle-cache.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo chown -R "$USER":"$USER" "$GRADLE_HOME"
-rm -rf "$GRADLE_HOME"/caches/*/plugin-resolution/
-ls -l "$GRADLE_HOME"/caches/ \
-  | grep --only-matching --perl-regexp "\d+\.\d+\.\d+\S*$" \
-  | xargs --replace rm -rf "$GRADLE_HOME"/caches/{}
-find "$GRADLE_HOME"/ -name "*.lock" -print0 | xargs -I {} rm -f {}
