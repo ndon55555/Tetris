0127c9ed326dcc1f390b254a259f44cc031515c3
Don Nguyen <ndon55555@gmail.com>
2020-12-24 02:39:31 -0500

    Revert "Revert "Revert "Revert "lint""""
    This reverts commit cdd1051dfa1bf6c77d56ba7e04bd55d945a35d7b.

--- core/src/js/controller/JSController.kt
+++ core/src/js/controller/JSController.kt
@@ -4,13 +4,16 @@ import kotlin.browser.window
 
 actual fun runAtFixedRate(period: Long, shouldContinue: () -> Boolean, event: () -> Unit) {
     var intervalId: Int? = null
-    intervalId = window.setInterval({
-        if (shouldContinue()) {
-            event()
-        } else {
-            intervalId?.let {
-                window.clearInterval(it)
+    intervalId = window.setInterval(
+        {
+            if (shouldContinue()) {
+                event()
+            } else {
+                intervalId?.let {
+                    window.clearInterval(it)
+                }
             }
-        }
-    }, period.toInt())
+        },
+        period.toInt()
+    )
 }
--- core/test/common/model/BoardTest.kt
+++ core/test/common/model/BoardTest.kt
@@ -4,12 +4,12 @@ import model.board.Board
 import model.board.BoardImpl
 import model.cell.CellColor
 import model.cell.CellImpl
+import kotlin.test.BeforeTest
+import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFails
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
-import kotlin.test.BeforeTest
-import kotlin.test.Test
 
 abstract class AbstractBoardTest {
     private lateinit var board: Board
@@ -23,23 +23,23 @@ abstract class AbstractBoardTest {
     @Test
     fun validCellsTest() {
         val valid = arrayOf(
-                CellImpl(CellColor.GREEN, 1, 0),
-                CellImpl(CellColor.GREEN, 0, 0),
-                CellImpl(CellColor.GREEN, 1, 1),
-                CellImpl(CellColor.GREEN, 2, 1)
+            CellImpl(CellColor.GREEN, 1, 0),
+            CellImpl(CellColor.GREEN, 0, 0),
+            CellImpl(CellColor.GREEN, 1, 1),
+            CellImpl(CellColor.GREEN, 2, 1)
         )
         assertTrue(board.areValidCells(*valid))
 
         val outOfBounds = arrayOf(
-                CellImpl(CellColor.GREEN, 1, 0),
-                CellImpl(CellColor.GREEN, 0, 0),
-                CellImpl(CellColor.GREEN, 0, -1),
-                CellImpl(CellColor.GREEN, -1, -1)
+            CellImpl(CellColor.GREEN, 1, 0),
+            CellImpl(CellColor.GREEN, 0, 0),
+            CellImpl(CellColor.GREEN, 0, -1),
+            CellImpl(CellColor.GREEN, -1, -1)
         )
         assertFalse(board.areValidCells(*outOfBounds))
 
         val overlapping = arrayOf(
-                CellImpl(CellColor.RED, 2, 1)
+            CellImpl(CellColor.RED, 2, 1)
         )
         board.placeCells(*valid)
         assertFalse(board.areValidCells(*overlapping))
@@ -50,13 +50,13 @@ abstract class AbstractBoardTest {
         assertEquals(0, board.getPlacedCells().size)
 
         val cells = arrayOf(
-                CellImpl(CellColor.RED, 1, 2),
-                CellImpl(CellColor.RED, 0, 0)
+            CellImpl(CellColor.RED, 1, 2),
+            CellImpl(CellColor.RED, 0, 0)
         )
         board.placeCells(*cells)
         val cells2 = arrayOf(
-                CellImpl(CellColor.RED, 2, 5),
-                CellImpl(CellColor.RED, 3, 4)
+            CellImpl(CellColor.RED, 2, 5),
+            CellImpl(CellColor.RED, 3, 4)
         )
         board.placeCells(*cells2)
         // Placed cells match cells of the original tetriminos
@@ -70,22 +70,24 @@ abstract class AbstractBoardTest {
     @Test
     fun clearLineTest() {
         val cells = arrayOf(
-                CellImpl(CellColor.RED, 1, 0),
-                CellImpl(CellColor.RED, 1, 1),
-                CellImpl(CellColor.RED, 1, 2),
-                CellImpl(CellColor.RED, 0, 2)
+            CellImpl(CellColor.RED, 1, 0),
+            CellImpl(CellColor.RED, 1, 1),
+            CellImpl(CellColor.RED, 1, 2),
+            CellImpl(CellColor.RED, 0, 2)
         )
         board.placeCells(*cells)
         board.clearLine(1)
         // All cells matching given row are removed
         assertEquals(1, board.getPlacedCells().size)
         // All cells above given row are moved down
-        assertTrue(board.getPlacedCells().contains(
+        assertTrue(
+            board.getPlacedCells().contains(
                 CellImpl(CellColor.RED, 0, 2).move(1, 0)
-        ))
+            )
+        )
     }
 }
 
-class BoardImplTest(): AbstractBoardTest() {
+class BoardImplTest() : AbstractBoardTest() {
     override fun boardImplementation(): Board = BoardImpl()
 }
